1af5f2d49ef334cb270010fb2d9dc211dfbd940a
hong-boy <hong-boy@foxmail.com>
2018-03-17 23:34:04 +0800

    refactor: 代码重构

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 'use strict';
-import Vue from 'vue'
-import ElementUI from 'element-ui'
-import NodeChartFlow from '../dist/bundle.js'
-import '../dist/bundle.css'
-import Layout from './demo/layout.vue'
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import NodeChartFlow from '../dist/bundle.js';
+import '../dist/bundle.css';
+import Layout from './demo/layout.vue';
 
 console.log(NodeChartFlow);
 
