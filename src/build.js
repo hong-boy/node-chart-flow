5ea7d65bcfc0e7aa72827b3861e1aac5c558ec2a
hong-boy <hong-boy@foxmail.com>
2018-03-27 23:31:48 +0800

    feat: release 0.0.12

--- src/build.js
+++ src/build.js
@@ -1,6 +1,6 @@
-import './editor/css/main.less'
+import './editor/css/main.less';
 
-import NodeChartFlow from './editor/Component.vue'
-import NodeType from './editor/NodeType.js'
+import NodeChartFlow from './editor/Component.vue';
+import NodeType from './editor/NodeType.js';
 
 export { NodeType, NodeChartFlow as default, };
